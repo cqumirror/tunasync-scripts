e5bed2b1eabf38262026cd0882debb56f491d34b
tedaimeng <tdm@tedaimeng.cn>
2026-05-21 20:29:09 +0800

    update crates.io-index.end.sh

--- crates.io-index.end.sh
+++ crates.io-index.end.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat > TUNASYNC_WORKING_DIR/config.json << 'EOF'
+cat > $TUNASYNC_WORKING_DIR/config.json << 'EOF'
 {
   "dl": "https://mirrors.cqu.edu.cn/crates.io/crates/{crate}/{crate}-{version}.crate",
   "api": "https://crates.io",
