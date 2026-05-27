16c55df419587b810a5de90c3d479e2090a3af46
tedaimeng <tdm@tedaimeng.cn>
2026-05-27 20:37:39 +0800

    update espressif configs

--- git-multirepo-recursive.sh
+++ git-multirepo-recursive.sh
@@ -5,7 +5,6 @@ SYNC_SCRIPT=${TUNASYNC_GIT_RECURSIVE_SCRIPT_PATH:-"./git-recursive.sh"}
 MIRROR_BASE_URL=${MIRROR_BASE_URL:-"https://mirrors.cqu.edu.cn/git/"}
 WORKING_DIR_BASE=${TUNASYNC_WORKING_DIR:-"/data/mirrors/git/"}
 
-exit_code=0
 jq -c '.repositories[]' "$CONFIG_FILE" | while read -r repo; do
     name=$(echo "$repo" | jq -r '.name')
     upstream=$(echo "$repo" | jq -r '.upstream')
@@ -24,10 +23,8 @@ jq -c '.repositories[]' "$CONFIG_FILE" | while read -r repo; do
     mkdir -p "$(dirname "$GENERATED_SCRIPT")"
 
     if ! "$SYNC_SCRIPT"; then
-        exit_code=$?
+        echo "Failed to sync" 
     fi
-    
-    echo "------------------------"
 done
-
-exit $exit_code
+total_size=$(du -sb "${WORKING_DIR_BASE}"|cut -f1)
+echo "Total size is" $(numfmt --to=iec $total_size)
