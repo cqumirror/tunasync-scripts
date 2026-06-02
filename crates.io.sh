d1db59da975c7b07b3efbdecd98e14b71780021a
tedaimeng <tdm@tedaimeng.cn>
2026-06-02 21:08:32 +0800

    correct script path

--- crates.io.sh
+++ crates.io.sh
@@ -14,10 +14,10 @@ mkdir -p "$INDEX_DIR" "$CRATES_DIR" "$STATE_DIR"
 # separate from crate tarballs to avoid unnecessary git and filesystem churn.
 # If any of these steps fail, the sync script shall be stopped.
 TO="$INDEX_DIR" /sync-crates-index.sh && \
-python3 ${_here}/sync-crates.py \
+python3 ${_here}/include/ustcmirror-images/crates-io/sync-crates.py \
     --index "$INDEX_DIR" \
     --crates "$CRATES_DIR" \
     --state "$STATE_DIR" && \
-python3 ${_here}/cleanup-crates.py \
+python3 ${_here}/include/ustcmirror-images/crates-io/cleanup-crates.py \
     --index "$INDEX_DIR" \
     --crates "$CRATES_DIR"
